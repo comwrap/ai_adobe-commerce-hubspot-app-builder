a845e1bb5700179848c272be7016b07d6bfdef2d
Ive Andric <ive@Ives-MacBook-Pro.local>
2024-11-26 10:24:43 +0100

    Add action updated

--- actions/customer/commerce/updated/sender.js
+++ actions/customer/commerce/updated/sender.js
@@ -9,7 +9,8 @@ the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTA
 OF ANY KIND, either express or implied. See the License for the specific language
 governing permissions and limitations under the License.
 */
-
+const {updateContact} = require("../../hubspot-api-client");
+const {Core} = require("@adobe/aio-sdk");
 /**
  * This function send the customer updated dara to the external back-office application
  *
@@ -22,9 +23,24 @@ async function sendData (params, data, preProcessed) {
   // @TODO Here add the logic to send the information to 3rd party
   // @TODO Use params to retrieve need parameters from the environment
   // @TODO in case of error return { success: false, statusCode: <error status code>, message: '<error message>' }
+  const logger = Core.Logger('customer-commerce-updated', {level: params.LOG_LEVEL || 'info'})
+  try {
+      const response = await updateContact(params.HUBSPOT_ACCESS_TOKEN, data, params.COMMERCE_HUBSPOT_CONTACT_ID_FIELD)
+      logger.debug('Hubspot response: ', response)
+      logger.debug('Contact id:', response.id)
 
-  return {
-    success: true
+      return {
+          success: true,
+          contactId: response.id
+      }
+
+  } catch (e) {
+      logger.error('There was an error updating Contact in HubSpot')
+      return {
+          success: false,
+          statusCode: 400,
+          message: e.response
+      }
   }
 }
 
--- actions/customer/hubspot-api-client.js
+++ actions/customer/hubspot-api-client.js
@@ -21,6 +21,23 @@ async function createContact(token, data) {
     return await hubspotClient.crm.contacts.basicApi.create(SimplePublicObjectInputForCreate);
 }
 
+async function updateContact(token, data, commerceHubspotIdField) {
+    const hubspotClient = new hubspot.Client({"accessToken": `${token}`});
+
+    const SimplePublicObjectInput = { associations: [], data };
+    const contactId = data[commerceHubspotIdField];
+
+    try {
+    const apiResponse = await hubspotClient.crm.contacts.basicApi.update(contactId, SimplePublicObjectInput);
+    console.log(JSON.stringify(apiResponse, null, 2));
+    } catch (e) {
+    e.message === 'HTTP request failed'
+        ? console.error(JSON.stringify(e.response, null, 2))
+        : console.error(e)
+    }
+}
+
 module.exports = {
-    createContact
+    createContact,
+    updateContact
 }
